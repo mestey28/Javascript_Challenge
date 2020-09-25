a716725dfcc2af04a7cd0391c58a05d8b2e90076
M Estey <smileyegg@gmail.com>
2020-09-24 18:17:35 -0600

    update js

--- static/js/app.js
+++ static/js/app.js
@@ -18,62 +18,85 @@ tableData.forEach((sighting) => {
 });
 
 
-// Form input 14.3.8
+// // Form input 14.3.8
 // // Select the button
-// var button = d3.select("#filter-btn)
+// var button = d3.select("#filter-btn")
 
 // // Select the form
-// var form = d3.select("#form")
+// var resetButton = d3.select("#reset-filter-btn")
 
 // // Create event handlers for clicking the button or pressing the enter key
 // button.on("click", runEnter);
-// form.on("submit",runEnter);
+// // resetButton.on("submit",runEnter);
 
-// ufoTable=d3.select("#ufo-table");
+// // remove any exisiting table
+// d3.select("tbody").html("");
 
-// tableData.array.forEach(sightings => {
-//   row=ufoTable.append('tr');
+// // prevent page from refreshing
+// // d3.event.preventDefault();
 
-//   Object.entries(sightings).forEach(([key,value]) =>{
-//     row.append('td').text(value);
-// });
-// });
+// // Select the input element and get the raw HTML node
+// var inputElement = d3.select("#datetime");
 
+// // Get the value property of the input element
+// var inputValue = inputElement.property("value");
 
+// // Print the value to the console
+// // console.log(inputValue);
 
-// // Create the function to run for both events
 // function runEnter() {
+// // Check if any data was entered in the filter box
+// // If no data was entered, exits the function
 
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
+//  if (inputValue === ""){
+//   // alert("No value entered");
+//   return console.log("No value entered");
+// }
 
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#datetime");
+   
+// //If there is something in the form, the function continues
+//   else{
 
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
+// /*Uses filter function and arrow function to run through the data
+// and stores the entries that match the input date in a new array*/
+// var filteredDate = tableData.filter(date => date.datetime === inputValue);
+//   console.log(filteredDate);
+        
+//   if ( filteredDate.length === 0){
+//     console.log("No data on that date exists");
+//     window.alert("No data exists on that date!");
+//     }
 
-//   // Print the value to the console
-//   console.log(inputValue);
+//   else {
+// //Reset the table on each click
+//         ufoTable.html("");
+            
+// //Add the header back to the empty table
+//         ufoTableHeader = ufoTable.append('thead').append('tr');
+//         ufoTableHeader.append('th').attr('class', 'table-head').text("Date");
+//         ufoTableHeader.append('th').attr('class', 'table-head').text("City");
+//         ufoTableHeader.append('th').attr('class', 'table-head').text("State");
+//         ufoTableHeader.append('th').attr('class', 'table-head').text("Country");
+//         ufoTableHeader.append('th').attr('class', 'table-head').text("Shape");
+//         ufoTableHeader.append('th').attr('class', 'table-head').text("Comments");
+
+// //Add tbody tag to append the filtered data
+//         ufoTable.append('tbody');
+
+// //Adds a row for each entry in the filtered data
+//         filteredDate.forEach(date => {
+//           row = ufoTable.select('tbody').append('tr');
+
+// //Runs through the values of each entry and adds them to the row
+//         Object.entries(date).forEach(([key,value]) => {
+//           row.append('td').text(value);
+//               })
+//             })
+//           }
+//         }
+// }
 
 
-//   // get table references
-// function buildTable(data) {
-//   // First, clear out any existing data
-//   // Next, loop through each object in the data
-//   // and append a row and cells for each value in the row
-//    // Append a row to the table body
-//    // Loop through each field in the dataRow and add
-//    // each value as a table cell (td)
-// //Create a fuction for handleClink()
-//   // Grab the datetime value from the filter
-//   // Check to see if a date was entered and filter the
-//   // data using that date.
-//    // Apply `filter` to the table data to only keep the
-//    // rows where the `datetime` value matches the filter value
-//   // Rebuild the table using the filtered data
-//   // @NOTE: If no date was entered, then filteredData will
-//   // just be the original tableData.
 
 
 
