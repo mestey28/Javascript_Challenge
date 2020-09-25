51838d9f948865f20c9462feb00ff27ae0017096
M Estey <smileyegg@gmail.com>
2020-09-24 18:48:26 -0600

    final update js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
+// write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+// Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
+//Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
 
 // from data.js
 var tableData = data;
@@ -17,84 +20,54 @@ tableData.forEach((sighting) => {
   });
 });
 
+// Form input 14.3.8
+// Select the button
+var button = d3.select("#filter-btn")
 
-// // Form input 14.3.8
-// // Select the button
-// var button = d3.select("#filter-btn")
-
-// // Select the form
+// Select the form
 // var resetButton = d3.select("#reset-filter-btn")
 
-// // Create event handlers for clicking the button or pressing the enter key
-// button.on("click", runEnter);
-// // resetButton.on("submit",runEnter);
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", function(){
 
-// // remove any exisiting table
-// d3.select("tbody").html("");
+
+// remove any exisiting table
+d3.select("tbody").html("");
 
 // // prevent page from refreshing
 // // d3.event.preventDefault();
 
-// // Select the input element and get the raw HTML node
-// var inputElement = d3.select("#datetime");
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
 
-// // Get the value property of the input element
-// var inputValue = inputElement.property("value");
+// Get the value property of the input element
+var inputValue = inputElement.property("value");
 
-// // Print the value to the console
-// // console.log(inputValue);
+// Print the value to the console
+console.log(inputValue);
 
-// function runEnter() {
-// // Check if any data was entered in the filter box
-// // If no data was entered, exits the function
+// filter data with datetimeequal to input value
+var filterData=tableData.filter(sighting=> sighting.datetime===inputValue);
+// print filter values
+console.log(filterData);
 
-//  if (inputValue === ""){
-//   // alert("No value entered");
-//   return console.log("No value entered");
-// }
+filterData.forEach(function(selections){
+  console.log(selections);
 
-   
-// //If there is something in the form, the function continues
-//   else{
+  // Append the row with 'tr' for each sighting
+  var row=tbody.append('tr');
 
-// /*Uses filter function and arrow function to run through the data
-// and stores the entries that match the input date in a new array*/
-// var filteredDate = tableData.filter(date => date.datetime === inputValue);
-//   console.log(filteredDate);
-        
-//   if ( filteredDate.length === 0){
-//     console.log("No data on that date exists");
-//     window.alert("No data exists on that date!");
-//     }
+  //Use Object.entries to console.log each UFO Sighting Value
+  Object.entries(selections).forEach(function([key, value]){
+    console.log(key, value);
 
-//   else {
-// //Reset the table on each click
-//         ufoTable.html("");
-            
-// //Add the header back to the empty table
-//         ufoTableHeader = ufoTable.append('thead').append('tr');
-//         ufoTableHeader.append('th').attr('class', 'table-head').text("Date");
-//         ufoTableHeader.append('th').attr('class', 'table-head').text("City");
-//         ufoTableHeader.append('th').attr('class', 'table-head').text("State");
-//         ufoTableHeader.append('th').attr('class', 'table-head').text("Country");
-//         ufoTableHeader.append('th').attr('class', 'table-head').text("Shape");
-//         ufoTableHeader.append('th').attr('class', 'table-head').text("Comments");
+    // Append a cell to each row
+    var cell=row.append('td');
+    cell.text(value);
+});
+});
+});         
 
-// //Add tbody tag to append the filtered data
-//         ufoTable.append('tbody');
-
-// //Adds a row for each entry in the filtered data
-//         filteredDate.forEach(date => {
-//           row = ufoTable.select('tbody').append('tr');
-
-// //Runs through the values of each entry and adds them to the row
-//         Object.entries(date).forEach(([key,value]) => {
-//           row.append('td').text(value);
-//               })
-//             })
-//           }
-//         }
-// }
 
 
 
